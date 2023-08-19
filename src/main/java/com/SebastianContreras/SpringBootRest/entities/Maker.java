f67d0bedd80849c3e62f6b0a29c5cf019745ecdf
Sebastian Contreras <sebasfac2017@gmail.com>
2023-08-19 19:23:24 -0300

    Creacion de entidades y mapeo de BD

---
+++ src/main/java/com/SebastianContreras/SpringBootRest/entities/Maker.java
@@ -0,0 +1,27 @@
+package com.SebastianContreras.SpringBootRest.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "fabricante")
+public class Maker {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String name;
+
+    @OneToMany(mappedBy = "maker",cascade = CascadeType.ALL,fetch = FetchType.LAZY,orphanRemoval = true)
+    private List<Product> productList= new ArrayList<>();
+}
---
+++ src/main/java/com/SebastianContreras/SpringBootRest/entities/Product.java
@@ -0,0 +1,29 @@
+package com.SebastianContreras.SpringBootRest.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "producto")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String name;
+
+    @Column(name = "precio")
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "id_fabricante",nullable = false)
+    private Maker maker;
+}
